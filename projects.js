0c4ebfbbbfe6e48120acccb65c0187631b72174e
Shuber <shuberinu@gmail.com>
2022-07-08 12:37:00 +0700

    initial commit

---
+++ projects.js
@@ -0,0 +1,53 @@
+const projects = [
+	{
+		id: 1,
+		name: 'FEM — QR Code Component',
+		summary: 'Frontend Mentor QR Code Component challenge solution.',
+		stack: 'HTML, CSS',
+		repo: 'https://github.com/sheronimo/frontendmentor-qrcode',
+		live: 'https://sheronimo.github.io/frontendmentor-qrcode/'
+	},
+	{
+		id: 2,
+		name: 'FEM — "Ping" Coming Soon Page',
+		summary: 'Frontend Mentor "Ping" coming soon page challenge solution.',
+		stack: 'HTML, SCSS, JS',
+		repo: 'https://github.com/sheronimo/frontendmentor-pingcomingsoon',
+		live: 'https://sheronimo.github.io/frontendmentor-pingcomingsoon/'
+	},
+	{
+		id: 3,
+		name: 'FEM — NFT Preview Card Component',
+		summary: 'Frontend Mentor NFT preview card component challenge solution.',
+		stack: 'HTML, CSS',
+		repo: 'https://github.com/sheronimo/frontendmentor-nftcard',
+		live: 'https://sheronimo.github.io/frontendmentor-nftcard/'
+	},
+	{
+		id: 4,
+		name: 'FEM — 3-Column Preview Card Component',
+		summary:
+			'Frontend Mentor 3-Column preview card component challenge solution.',
+		stack: 'HTML, SCSS',
+		repo: 'https://github.com/sheronimo/frontendmentor-3colcard',
+		live: 'https://sheronimo.github.io/frontendmentor-3colcard/'
+	},
+	{
+		id: 5,
+		name: 'FEM — Advice Generator App',
+		summary: 'Frontend Mentor Advice Generator app challenge solution.',
+		stack: 'HTML, CSS, JS',
+		repo: 'https://github.com/sheronimo/frontendmentor-advicegenerator',
+		live: 'https://sheronimo.github.io/frontendmentor-advicegenerator/'
+	},
+	{
+		id: 6,
+		name: 'FEM — Four Card Feature Section',
+		summary: 'Frontend Mentor Four Card feature section challenge solution.',
+		stack: 'HTML, SCSS',
+		repo: 'https://github.com/sheronimo/frontendmentor-fourcard',
+		live: 'https://sheronimo.github.io/frontendmentor-fourcard/'
+	}
+];
+
+export default projects;
