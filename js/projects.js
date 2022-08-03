19fd9c32917f0405fc6ab4ea84843acb69f1a54e
Shuber <shuberinu@gmail.com>
2022-08-03 12:38:04 +0700

    add more projects

--- js/projects.js
+++ js/projects.js
@@ -2,7 +2,7 @@ export const projects = [
 	{
 		id: 1,
 		name: 'FEM — QR Code Component',
-		summary: 'Frontend Mentor QR Code Component challenge solution.',
+		summary: 'Frontend Mentor challenge testing building responsive layouts.',
 		stack: 'HTML, CSS',
 		repo: 'https://github.com/sheronimo/frontendmentor-qrcode',
 		live: 'https://sheronimo.github.io/frontendmentor-qrcode/'
@@ -10,7 +10,8 @@ export const projects = [
 	{
 		id: 2,
 		name: 'FEM — "Ping" Coming Soon Page',
-		summary: 'Frontend Mentor "Ping" coming soon page challenge solution.',
+		summary:
+			'Frontend Mentor challenge testing simple form validation of an email field.',
 		stack: 'HTML, SCSS, JS',
 		repo: 'https://github.com/sheronimo/frontendmentor-pingcomingsoon',
 		live: 'https://sheronimo.github.io/frontendmentor-pingcomingsoon/'
@@ -18,7 +19,8 @@ export const projects = [
 	{
 		id: 3,
 		name: 'FEM — NFT Preview Card Component',
-		summary: 'Frontend Mentor NFT preview card component challenge solution.',
+		summary:
+			'Frontend Mentor challenge testing building responsive layouts and different types of positioning.',
 		stack: 'HTML, CSS',
 		repo: 'https://github.com/sheronimo/frontendmentor-nftcard',
 		live: 'https://sheronimo.github.io/frontendmentor-nftcard/'
@@ -27,7 +29,7 @@ export const projects = [
 		id: 4,
 		name: 'FEM — 3-Column Preview Card Component',
 		summary:
-			'Frontend Mentor 3-Column preview card component challenge solution.',
+			'Frontend Mentor challenge testing responsive design using Flex and/or Grid.',
 		stack: 'HTML, SCSS',
 		repo: 'https://github.com/sheronimo/frontendmentor-3colcard',
 		live: 'https://sheronimo.github.io/frontendmentor-3colcard/'
@@ -35,7 +37,8 @@ export const projects = [
 	{
 		id: 5,
 		name: 'FEM — Advice Generator App',
-		summary: 'Frontend Mentor Advice Generator app challenge solution.',
+		summary:
+			'Frontend Mentor challenge testing 3rd party API interaction knowledge. Uses Advice Slip API to generate random quotes of advice.',
 		stack: 'HTML, CSS, JS',
 		repo: 'https://github.com/sheronimo/frontendmentor-advicegenerator',
 		live: 'https://sheronimo.github.io/frontendmentor-advicegenerator/'
@@ -43,7 +46,8 @@ export const projects = [
 	{
 		id: 6,
 		name: 'FEM — Four Card Feature Section',
-		summary: 'Frontend Mentor Four Card feature section challenge solution.',
+		summary:
+			'Frontend Mentor challenge testing multi-column responsive layouts.',
 		stack: 'HTML, SCSS',
 		repo: 'https://github.com/sheronimo/frontendmentor-fourcard',
 		live: 'https://sheronimo.github.io/frontendmentor-fourcard/'
@@ -52,7 +56,7 @@ export const projects = [
 		id: 7,
 		name: 'SNH48G Budgeter App',
 		summary:
-			'Simple app for calculating how many SNH48 Election votes you can buy with a specified budget and currency',
+			'Simple app for calculating how many SNH48 Election votes you can buy with a specified budget and currency. Uses a currency converter API.',
 		stack: 'React',
 		repo: 'https://github.com/sheronimo/react-snh48g-budgeter-app',
 		live: 'https://snh48g-budgeter.netlify.app/'
@@ -60,9 +64,62 @@ export const projects = [
 	{
 		id: 8,
 		name: 'Quizzical',
-		summary: 'Trivia quiz game app, with questions from the Open Trivia DB',
+		summary:
+			'Trivia quiz game app, with questions from the Open Trivia DB API.',
 		stack: 'React',
 		repo: 'https://github.com/sheronimo/react-quizzical',
 		live: 'https://quizzical-shuber.netlify.app'
+	},
+	{
+		id: 9,
+		name: 'FEM — Testimonials Grid Section',
+		summary: 'Frontend Mentor challenge testing CSS Grid skills.',
+		stack: 'HTML, CSS',
+		repo: 'https://github.com/sheronimo/frontendmentor-testimonialsgrid',
+		live: 'https://sheronimo.github.io/frontendmentor-testimonialsgrid/'
+	},
+	{
+		id: 10,
+		name: 'FEM — Social Proof Section',
+		summary:
+			'Frontend Mentor challenge testing cascading design using Flex or Grid.',
+		stack: 'HTML, SCSS',
+		repo: 'https://github.com/sheronimo/frontendmentor-socialproof',
+		live: 'https://sheronimo.github.io/frontendmentor-socialproof/'
+	},
+	{
+		id: 11,
+		name: 'FEM — Single Price Grid Component',
+		summary:
+			'Frontend Mentor challenge testing a responsive grid layout. Built mobile-first.',
+		stack: 'HTML, SCSS',
+		repo: 'https://github.com/sheronimo/frontendmentor-singlepricegrid',
+		live: 'https://sheronimo.github.io/frontendmentor-singlepricegrid/'
+	},
+	{
+		id: 12,
+		name: 'FEM — FAQ Accordion Card',
+		summary: 'Frontend Mentor challenge testing an accordion design pattern.',
+		stack: 'HTML, SCSS, JS',
+		repo: 'https://github.com/sheronimo/frontendmentor-faqaccordion',
+		live: 'https://sheronimo.github.io/frontendmentor-faqaccordion/'
+	},
+	{
+		id: 13,
+		name: 'FEM — Interactive Rating Component',
+		summary:
+			'Frontend Mentor challenge testing user interaction and updating the DOM.',
+		stack: 'HTML, CSS, JS',
+		repo: 'https://github.com/sheronimo/frontendmentor-ratingcomponent',
+		live: 'https://sheronimo.github.io/frontendmentor-ratingcomponent/'
+	},
+	{
+		id: 14,
+		name: 'FEM — Intro Component with Sign-Up Form',
+		summary:
+			'Frontend Mentor challenge testing sign-up form validation and two-column layout.',
+		stack: 'HTML, SCSS, JS',
+		repo: 'https://github.com/sheronimo/frontendmentor-introcomponentwithform',
+		live: 'https://sheronimo.github.io/frontendmentor-introcomponentwithform/'
 	}
 ];
